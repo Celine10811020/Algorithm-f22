900ffa557f385af9e448055cbf3f5f7a06200708
Celine10811020 <86948988+Celine10811020@users.noreply.github.com>
2022-10-10 19:44:46 +0800

    Update Queries.c

--- HW_1/Queries/Queries.c
+++ HW_1/Queries/Queries.c
@@ -3,39 +3,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void mergeSort(int *queue, int head, int tail);
-void merge(int *queue, int head, int mid, int tail);
+void mergeSort(int head, int tail);
+void merge(int head, int mid, int tail);
+
+int queue[200000] = {};
+int queries[200000][2] = {};
 
 int main()
 {
     int i, num, queueNum, temp, home, end, remain=0;
-    int *queue = NULL;
-    int **queries = NULL;
+    //int *queue = NULL;
+    //int **queries = NULL;
 
     scanf("%d", &num);
 
-    queries = (int**)malloc(sizeof(int*)*num); //num*2
+    /*queries = (int**)malloc(sizeof(int*)*num); //num*2
     for(i=0; i<num; i++)
     {
         queries[i] = (int*)malloc(sizeof(int)*2);
-    }
+    }*/
 
-    queueNum = 0;
-    remain = 0;
     for(i=0; i<num; i++)
     {
-        scanf("%d", &temp);
-        queries[i][0] = temp;
-        if(temp == 1)
+        scanf("%d", &queries[i][0]);
+        if(queries[i][0] == 1)
         {
-            scanf("%d", &temp);
-            queries[i][1] = temp;
-            queueNum++;
-            remain++;
+            scanf("%d", &queries[i][1]);
         }
     }
 
-    queue = (int*)malloc(sizeof(int)*queueNum);
+    //queue = (int*)malloc(sizeof(int)*queueNum);
     home = 0; //輸入的位置（Last In）
     end = 0; //最開始的位置（First Out）
     remain = 0; //queue中有幾個數字
@@ -49,7 +46,7 @@ int main()
             remain++; //queue中數字++
         }else if(queries[i][0] == 2)
         {
-            mergeSort(queue, end, home-1);
+            mergeSort(end, home-1);
         }else
         {
             if(remain == 0) //如果queue中沒有東西
@@ -64,23 +61,25 @@ int main()
         }
     }
 
-    for(i=0; i<num; i++)
+    /*for(i=0; i<num; i++)
     {
         free(queries[i]);
     }
     free(queries);
-    free(queue);
+    free(queue);*/
 
     return 0;
 }
 
+int leftSub[100001];
+int rightSub[100001];
 
-void merge(int *queue, int head, int mid, int tail)
+void merge(int head, int mid, int tail)
 {
     int lenA = mid - head + 1;
     int lenB = tail - (mid+1) + 1;
-    int *leftSub = (int*)malloc(sizeof(int)*(lenA+1));
-    int *rightSub = (int*)malloc(sizeof(int)*(lenB+1));
+    //int *leftSub = (int*)malloc(sizeof(int)*(lenA+1));
+    //int *rightSub = (int*)malloc(sizeof(int)*(lenB+1));
 
     int leftIndex = 0;
     int rightIndex = 0;
@@ -112,18 +111,18 @@ void merge(int *queue, int head, int mid, int tail)
         }
     }
 
-    free(leftSub);
-    free(rightSub);
+    //free(leftSub);
+    //free(rightSub);
 }
 
-void mergeSort(int *queue, int head, int tail)
+void mergeSort(int head, int tail)
 {
     int mid=0;
     if(head < tail)
     {
         mid = (head+tail) / 2;
-        mergeSort(queue, head, mid);
-        mergeSort(queue, mid+1, tail);
-        merge(queue, head, mid, tail);
+        mergeSort(head, mid);
+        mergeSort(mid+1, tail);
+        merge(head, mid, tail);
     }
 }
